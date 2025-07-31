cec9916ae2a1d179e2dcbdac1bd3697278b7754a
rhpessoa <rhpessoa29@gmail.com>
2025-07-31 13:26:13 -0300

    feat: add Home component

--- src/app/home/home.component-module.ts
+++ src/app/home/home.component-module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { HOME_ROUTES } from './home.component-routing-module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
-    CommonModule,
     HomeComponent,
     RouterModule.forChild(HOME_ROUTES)
   ]
