3be97f45fb22ac6ffc1c548cde8549c78f76dcfd
rhpessoa <rhpessoa29@gmail.com>
2025-07-31 09:21:47 -0300

    feat: implement desktop-only version of the header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { SharedMaterialModule } from "../shared-material.module";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SharedMaterialModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class Header {
+export class HeaderComponent {
+menuAberto = false;
+constructor() {}
 
+ngOnInit() {
+
+}
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
 }
