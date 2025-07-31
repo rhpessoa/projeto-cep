4b6d29694308c4ef26068ec618fce0cd12652e57
rhpessoa <rhpessoa29@gmail.com>
2025-07-31 16:09:14 -0300

    feat: add responsive menu for mobile in header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,14 +8,27 @@ import { SharedMaterialModule } from "../shared-material.module";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-menuAberto = false;
+ menuAberto = false;
+  menuMobileAberto = false;
+  menuMobileEnderecosAberto = false;
 constructor() {}
 
 ngOnInit() {
 
 }
 
-  toggleMenu() {
+ toggleMenu() {
     this.menuAberto = !this.menuAberto;
+  }
+
+  toggleMenuMobile() {
+    this.menuMobileAberto = !this.menuMobileAberto;
+    if (!this.menuMobileAberto) {
+      this.menuMobileEnderecosAberto = false;
+    }
+  }
+
+  toggleMenuMobileEnderecos() {
+    this.menuMobileEnderecosAberto = !this.menuMobileEnderecosAberto;
   }
 }
